a1b523fce155431a6ecfecde652f6676431a2ae6
Chao Taphy <taphylili@gmail.com>
2023-04-06 15:04:48 +0800

    decoration

--- src/App.tsx
+++ src/App.tsx
@@ -30,25 +30,29 @@ function App() {
   if (questions?.isQuestion) {
     return (
       <div className="App">
-        <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
-        <div key={questions?.id}>
-          <h2>{questions?.question}</h2>
-          <ul>
-            {questions?.options && questions?.options.map((option, index) => (
-              <li key={option}>
-                {questions?.answers && <a href={`?questionID=${questions?.answers[index]}`}>{option}</a>}
-              </li>
-            ))}
-          </ul>
+        <div className="app-container">
+          <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
+          <div key={questions?.id}>
+            <h2>{questions?.question}</h2>
+            <ul>
+              {questions?.options && questions?.options.map((option, index) => (
+                <li key={option}>
+                  {questions?.answers && <a href={`?questionID=${questions?.answers[index]}`}>{option}</a>}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     );
   } else {
     return (
-      <div className="App">+1
-        <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
-        <h2>{questions?.answer}</h2>
-        {questions?.answer && <a href={`?questionID=${questions?.next}`}>{`請回到第${questions?.next && questions?.next + 1}題`}</a>}
+      <div className="App">
+        <div className="app-container">
+          <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
+          <h2>{questions?.answer}</h2>
+          {questions?.answer && <a href={`?questionID=${questions?.next}`}>{`請回到第${questions?.next && questions?.next + 1}題`}</a>}
+        </div>
       </div>
     );
   }
