d6f06df9584765a58cc896393a0f741f2ad9fd19
Chao Taphy <taphylili@gmail.com>
2023-04-11 20:04:44 +0800

    separate

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,14 @@ interface Question {
   next?: number;
 }
 
+interface AnswerProps {
+  answer?: Question;
+}
+
+interface QuestionProps {
+  question?: Question;
+}
+
 function App() {
 
   const [questions, setQuestions] = useState<Question>();
@@ -27,32 +35,47 @@ function App() {
   if (questions?.isQuestion) {
     return (
       <div className="App">
-        <div className="app-container">
-          <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
-          <div key={questions?.id}>
-            <h2>{questions?.question}</h2>
-            <ul>
-              {questions?.options && questions?.options.map((option, index) => (
-                <li key={option}>
-                  {questions?.answers && <a href={`?questionID=${questions?.answers[index]}`}>{option}</a>}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <QestionPage question={questions} />
       </div>
     );
   } else {
     return (
       <div className="App">
-        <div className="app-container">
-          <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
-          <h2>{questions?.answer}</h2>
-          {questions?.answer && <a href={`?questionID=${questions?.next}`}>{`請回到第${questions?.next && questions?.next + 1}題`}</a>}
-        </div>
+        <AnswerPage answer={questions} />
       </div>
     );
   }
 }
+
+
+
+function AnswerPage(props: AnswerProps) {
+  return (
+    <div className="app-container">
+      <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
+      <h2>{props.answer?.answer}</h2>
+      {props.answer?.answer && <a href={`?questionID=${props.answer?.next}`}>{`請回到第${props.answer?.next && props.answer?.next + 1}題`}</a>}
+    </div>
+  )
+}
+
+function QestionPage(props: QuestionProps) {
+  return (
+    <div className="app-container">
+      <h1>心理測驗：歡迎來到輪迴小遊戲！</h1>
+      <div key={props.question?.id}>
+        <h2>{props.question?.question}</h2>
+        <ul>
+          {props.question?.options && props.question?.options.map((option, index) => (
+            <li key={option}>
+              {props.question?.answers && <a href={`?questionID=${props.question?.answers[index]}`}>{option}</a>}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
 
 export default App;
